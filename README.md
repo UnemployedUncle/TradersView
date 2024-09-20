9dbe56d28da33bf34bbee65fd0916138d7cc2828
YONGSUP.YI <supsupbee@gmail.com>
2024-09-20 17:29:44 +0900

    Updated readme with todos

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # TradersView
+
+## Target
+  - AI industry study
+  - Time-series data engineering (running system)
+  - Prediction DL models (AIOps)
+
+## To-do (Routine building)
+- [ ] Process draft (Prediction)
+- [ ] Add functions (Internal learderboard)
+    - [ ] 1. Data cleaning
+        - [ ] API
+        - [ ] Cleaning
+        - [ ] DB
+    - [ ] 2. Data Analysis
+    - [ ] 3. Data Visualization
+    - [ ] 4. Fundamental ananlysis - F/S, news
+    - [ ] 5. Modeling - MA, Bollinger bands, ML, sentiment, DL ...
---
+++ lab/test_api.py
@@ -0,0 +1,44 @@
+"""
+## Stock market API
+
+1. Pricing - Free, API call limits
+2. Data coverage - Stock(price, volume), Company financials, News, Historical data
+3. (near) Real-time data
+4. Documentation (Support), Policies
+
+
+https://medium.com/coinmonks/best-stock-market-apis-ae1efb739ac4
+
+A.
+
+IEX Cloud API - 100call/day
+https://iexcloud.io/docs/api/
+
+Yahoo Finance API - Free
+https://developer.yahoo.com/api/
+
+
+B.
+
+Polygon.io  - Free, 5call/min
+https://polygon.io/pricing
+
+Tiingo API - Free, 1000call/day
+https://www.tiingo.com/
+
+
+C.
+
+Alpha Vantage API - 500call/day
+https://www.alphavantage.co/
+
+Quandl API - 500call/day
+https://data.nasdaq.com/publishers/QDL
+
+
+"""
+
+
+
+
+
