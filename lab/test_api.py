09777e3f698b1b9ed325b1f87ba31889450d71d8
UnemployedUncle <supsupbee@gmail.com>
2024-09-21 22:22:15 +0900

    Yahoo finance api

--- lab/test_api.py
+++ lab/test_api.py
@@ -11,11 +11,11 @@ https://medium.com/coinmonks/best-stock-market-apis-ae1efb739ac4
 
 A.
 
-IEX Cloud API - 100call/day
-https://iexcloud.io/docs/api/
-
 Yahoo Finance API - Free
 https://developer.yahoo.com/api/
+
+IEX Cloud API - 100call/day
+https://iexcloud.io/docs/api/
 
 
 B.
@@ -38,7 +38,25 @@ https://data.nasdaq.com/publishers/QDL
 
 """
 
+# A. Yahoo Finance API
+# pip install yfinance
 
 
+import yfinance as yf
+from datetime import datetime
 
+ticker = "AAPL"
+
+# Daily
+result = yf.Ticker(ticker)
+# print(result.info)
+
+# period: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max (optional, default is '1mo')
+# interval: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo(optional, default is '1d')
+apple = yf.download(tickers="AAPL", period="5d", interval="1m")
+
+start = datetime(2024, 8, 1, 9, 30, 0)
+end = datetime(2024, 8, 1, 11, 30, 0)
+
+filtered = apple[start: end]
 
